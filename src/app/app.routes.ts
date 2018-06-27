79f0212269d0586db74b86e750e733c7b5ddd9c4
ujjwal <ujjwalgupta983@gmail.com>
2018-06-27 21:58:26 +0530

    first commit

---
+++ src/app/app.routes.ts
@@ -0,0 +1,15 @@
+import { LayoutComponent } from './layout/layout.component';
+import { RegisterComponent } from './register/register.component';
+import { HomeComponent } from './home/home.component';
+import { RouterModule, Routes } from '@angular/router';
+
+import { Component } from '@angular/core';
+
+
+export const rootRouterConfig: Routes = [
+ {path:'', component:HomeComponent},
+ {path:'register', component:RegisterComponent},
+ {path:'layout',component:LayoutComponent}
+];
+export const routing = RouterModule.forRoot(rootRouterConfig);
+ 
